b0a87f3292fbd82859a8e257849a1d44b9b7d43d
Brandon Perkins <bperkins@redhat.com>
2013-02-20 15:50:28 -0500

    Add LWP::Simple - simple procedural interface to LWP.

---
+++ http-client-tests/LWP-Simple.pl
@@ -0,0 +1,71 @@
+#! /usr/bin/perl -wT
+
+use Regexp::Common qw /URI/;
+use strict;
+use Time::HiRes qw(gettimeofday);
+use URI::Split qw(uri_split uri_join);
+use warnings;
+use LWP::Simple;
+
+my $uri = "";
+if ( @ARGV < 1 || @ARGV > 1 ) {
+    die "Usage: $0 [URI]: $!\n";
+}
+else {
+    $uri = $ARGV[0];
+    chomp($uri);
+}
+
+#print "URI: $uri\n";
+
+if ( $uri =~ m/$RE{URI}{HTTP}/ ) {
+
+    #print "'$uri' IS a valid HTTP URI.\n";
+}
+else {
+    die "'$uri' is NOT a valid HTTP URI: $!\n";
+}
+
+( my $scheme, my $auth, my $path, my $query, my $frag ) = uri_split($uri);
+
+#printf("scheme: %-40s\n  auth: %-40s\n  path: %-40s\n query: %-40s\n  frag: %-40s\n", $scheme, $auth, $path, $query, $frag );
+
+my $size = 0;
+my $time = 0;
+
+my $ua = LWP::UserAgent->new;
+$ua->agent("MyApp/0.1 ");
+
+# Create the URL
+my $url = $scheme . "://" . $auth . $path;
+
+( my $s0, my $usec0 ) = gettimeofday();
+
+# Pass request to the user agent and get a response back
+my $res = get($url);
+( my $s1, my $usec1 ) = gettimeofday();
+
+# Check the outcome of the response
+if ( $res ) {
+    print "SUCCESS!\n";
+    $size = length( $res );
+}
+else {
+    print "FAILURE!\n";
+}
+
+my $selapsed        = $s1 - $s0;
+my $usecelapsed     = $usec1 - $usec0;
+my $stomselapsed    = ( $selapsed * 1000 );
+my $usectomselapsed = ( $usecelapsed / 1000 );
+my $mselapsed       = $stomselapsed + $usectomselapsed;
+
+$time = $mselapsed;
+
+my $rate = ( ( ( $size * 8 ) / 1000000 ) / ( $time / 1000 ) );
+
+print "SIZE: $size bytes\n";
+print "TIME: $time ms\n";
+print "RATE: $rate Mbps\n";
+
+exit 0;
